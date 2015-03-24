20b7f71091cc365e59a2253f10df54b18a0e769b
Xpd1 <paldejan@hotmail.com>
2015-03-24 19:35:37 +0100

    Updates
    Shitload of updates

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 __author__ = 'Xpd'
-back = "C:/Users/Unknown/PycharmProjects/untitled5/bg.png"
-sh = "C:/Users/Unknown/PycharmProjects/untitled5/ship.png"
-ali = "C:/Users/Unknown/PycharmProjects/untitled5/alien.png"
+back = "bg.jpg"
+sh = "ship.png"
+ali = "alien.png"
 import pygame
 import random
 import sys
@@ -13,18 +13,19 @@ green =(0,255,0)
 blue = (0,0,255)
 display_width = 800
 display_height = 600
+if pygame.image.get_extended() == 0:
+    print("Error: SDL_image required. Exiting.")
+elif pygame.image.get_extended() == 1:
+    print("Working")
 
-
-background = pygame.image.load(back)
-ship = pygame.image.load(sh)
-alien = pygame.image.load(ali)
 clock = pygame.time.Clock()
-
+#Nigga
 gameDisplay = pygame.display.set_mode((display_width,display_height), 0, 0)
 pygame.display.set_caption('Invaders')
 def Intro():
     Intro = True
     while Intro == True:
+    while Intro is True:
         gameDisplay.fill(green)
         pygame.display.update()
         for event in pygame.event.get():
@@ -34,19 +35,9 @@ def Intro():
             if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-def text_to_button(msg, color, buttonx, buttony, buttonwidth, buttonheight, size = "small"):
-    textSurf, textRect = text_objects(msg,color,size)
-    textRect.center = ((buttonx+(buttonwidth/2)), buttony+(buttonheight/2))
-    gameDisplay.blit(textSurf, textRect)
-
-    return textSurface, textSurface.get_rect()
-
-
-
-def text(msg,color, y_displace=0, size = "small"):
-    textSurf, textRect = text_objects(msg,color, size)
-    textRect.center = (display_width / 2), (display_height / 2)+y_displace
-    gameDisplay.blit(textSurf, textRect)
+def Images(pic,x,y ):
+    picture = pygame.image.load(pic)
+    gameDisplay.blit(picture, (x,y))
 
 def GameLoop():
     gameExit = False
@@ -68,7 +59,7 @@ def GameLoop():
     movement = 0
 
 
-    while gameOver == True:
+    while gameOver is True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
@@ -123,17 +114,16 @@ def GameLoop():
         prime_x += movement
         bullet_y += bulletspeed
         pygame.draw.rect(gameDisplay, green,[alien_x,alien_y,50 ,50 ] )
-        gameDisplay.blit(background, (0,0))
-        gameDisplay.blit(alien, (alien_x,alien_y))
-
-        gameDisplay.blit(ship, (prime_x-22,545))
+        Images(back, 0, 0)
+        Images(ali, alien_x, alien_y)
+        Images(sh ,prime_x-22, 545)
 
         pygame.draw.rect(gameDisplay, red, [bullet_x, bullet_y,bullet_size, bullet_size])
         pygame.display.update()
         clock.tick(fps)
 
 
-Intro()
+#Intro()
 GameLoop()
 pygame.quit()
 sys.exit()
